9a43bd5b7a252379e9aee4916cde9bd15d0948ab
chika0408 <akichica5@gmail.com>
2026-01-21 22:55:35 +0900

    gemini更新用

--- bin/train_model.py
+++ bin/train_model.py
@@ -22,25 +22,32 @@ df.columns = [
     'input_right_hand_dist_ave','input_left_hand_dist_ave',
     'input_head_dist_ave',
     'input_ChestVal',
+    'input_moving_ratio',
+    'input_interaction',
     'target_kire',
     'target_furi_0', 'target_furi_1', 'target_furi_2', 'target_furi_3',
-    'target_furi_4', 'target_furi_5', 'target_furi_6'
+    'target_furi_4', 'target_furi_5', 'target_furi_6',
+    'target_bz_1x', 'target_bz_1y',
+    'target_bz_2x', 'target_bz_2y'
 ]
 
 X = df[['input_sharpness', 'input_magnitude',
         'input_right_foot_dist_ave','input_left_foot_dist_ave',
         'input_right_hand_dist_ave','input_left_hand_dist_ave',
         'input_head_dist_ave',
-        'input_ChestVal']]
+        'input_ChestVal',
+        'input_moving_ratio',
+        'input_interaction']]
 
 # 学習と書き出し
 with open('model_params.txt', 'w') as f:
     # kire の学習
     model_kire = LinearRegression()
     model_kire.fit(X, df['target_kire'])
-    # 係数8つと切片1つを書き出す (スペース区切り)
+    # 係数10つと切片1つを書き出す (スペース区切り)
     f.write(f"{model_kire.coef_[0]} {model_kire.coef_[1]} {model_kire.coef_[2]} {model_kire.coef_[3]} \
             {model_kire.coef_[4]} {model_kire.coef_[5]} {model_kire.coef_[6]} {model_kire.coef_[7]} \
+            {model_kire.coef_[8]} {model_kire.coef_[9]} \
             {model_kire.intercept_}\n")
 
     # furi[0]～furi[6] の学習
@@ -50,6 +57,17 @@ with open('model_params.txt', 'w') as f:
         model_furi.fit(X, df[target_col])
         f.write(f"{model_furi.coef_[0]} {model_furi.coef_[1]} {model_furi.coef_[2]} {model_furi.coef_[3]} \
                 {model_furi.coef_[4]} {model_furi.coef_[5]} {model_furi.coef_[6]} {model_furi.coef_[7]} \
+                {model_furi.coef_[8]} {model_furi.coef_[9]} \
                 {model_furi.intercept_}\n")
+
+    # ベジェ曲線の制御点の学習
+    for i in range(4):
+        target_col = f'target_bz_{i}'
+        model_bz = LinearRegression()
+        model_bz.fit(X, df[target_col])
+        f.write(f"{model_bz.coef_[0]} {model_bz.coef_[1]} {model_bz.coef_[2]} {model_bz.coef_[3]} \
+                {model_bz.coef_[4]} {model_bz.coef_[5]} {model_bz.coef_[6]} {model_bz.coef_[7]} \
+                {model_bz.coef_[8]} {model_bz.coef_[9]} \
+                {model_bz.intercept_}\n")
 
 print("学習完了: model_params.txt を更新しました。")
